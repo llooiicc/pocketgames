98d14aa55411694cebec45d24ae7b6981b1d9d38
Loic.Rondel <loic.rondel@gmail.om>
2018-03-05 17:41:50 +0100

    ajout d'onglets dans la vue /games , ajout d'un player dans la vue /player

--- public/javascripts/gameLuncher.js
+++ public/javascripts/gameLuncher.js
@@ -1,14 +1,35 @@
-var h = window.innerHeight;
-var w = window.innerWidth;
-
 var header = document.querySelector('.header');
-var headerH = header.offsetHeight;
-
-var iframeH = h - headerH;
-
 var iframe = document.querySelector('#game-frame');
 
-iframe.height = iframeH;
+resizeIframe();
+
+window.addEventListener('resize', resizeIframe);
+
+$.getJSON('/games.json', function (datas) {
+
+    console.log(datas);
+    datas['games'].forEach(function (game) {
+
+        if(iframe.name === game.name){
+             console.log(game);
+             iframe.src = "http://" + game.embeded;
+        }
+
+    })
+});
+
+function resizeIframe(){
+
+    var h = window.innerHeight;
+    var w = window.innerWidth;
+
+    var headerH = header.offsetHeight;
+    var iframeH = h - headerH;
+
+    // iframe.height = iframeH;
+    iframe.height = innerHeight;
+
+}
 
 
 
--- public/javascripts/gamesInjector.js
+++ public/javascripts/gamesInjector.js
@@ -1,5 +1,19 @@
-var gamesContainer = document.querySelector('#og-grid');
-var globalContainer = document.querySelector('.wrap');
+var gamesContainers =
+    {
+        action: document.querySelector('#action-pane'),
+        shooting: document.querySelector('#shooting-pane'),
+        race: document.querySelector('#race-pane'),
+        arcade: document.querySelector('#arcade-pane')
+    };
+
+var tabs =
+    {
+        action: document.querySelector('#tab-action'),
+        shooting: document.querySelector('#tab-shooting'),
+        race: document.querySelector('#tab-race'),
+        arcade: document.querySelector('#tab-arcade')
+    };
+
 
 $.getJSON('games.json', function (datas) {
 
@@ -25,7 +39,54 @@ function injectGame(game){
         .replace('{{img-src}}', game.assets[0])
         .replace('{{game-name}}', game.name);
 
-    gamesContainer.innerHTML += liModel;
+    document.querySelector('#og-grid').innerHTML += liModel;
 
     return 0;
+}
+
+setTabsListener();
+function setTabsListener(){
+
+    tabs.action.firstChild.addEventListener('click', function (evt) { activeTab(tabs.action) });
+    tabs.shooting.firstChild.addEventListener('click', function (evt) { activeTab(tabs.shooting) });
+    tabs.arcade.firstChild.addEventListener('click', function (evt) { activeTab(tabs.arcade) });
+    tabs.race.firstChild.addEventListener('click', function (evt) { activeTab(tabs.race) });
+
+}
+
+function unactiveAllTabs() {
+
+    for(var k in tabs){
+        tabs[k].className = "unactive";
+    }
+
+    reduceAllPanes();
+}
+
+function activeTab(tab){
+    unactiveAllTabs();
+    tab.className = "active";
+
+    switch (tab.id) {
+        case "tab-action":
+            openPane(gamesContainers.action);
+        case "tab-shooting":
+            openPane(gamesContainers.shooting);
+        case "tab-arcade":
+            openPane(gamesContainers.arcade);
+        case "tab-race":
+            openPane(gamesContainers.race);
+    }
+}
+
+function reduceAllPanes(){
+
+    gamesContainers.action.style.height = "0px";
+    gamesContainers.action.style.overflow = "hidden";
+
+}
+
+function openPane(pane){
+
+    pane.style.height = "auto";
 }
