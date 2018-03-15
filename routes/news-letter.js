c77830edc2b9cb69044109bbc26f138e116b7ed8
loic <loic.rondel@gmail.com>
2018-03-15 13:20:39 +0100

    ajout d'une base sqlite3 gestion de l'inscription a la news letter

--- routes/news-letter.js
+++ routes/news-letter.js
@@ -1,14 +1,47 @@
-var express = require('express');
-var router = express.Router();
+let DbAcces = require('../bin/datas/DbAcces');
+let dbAcces = new DbAcces();
+
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router
     .get('/', function (req, res, next) {
         res.render('news-letter', {
             title: 'stay connect',
-            active: 'none'
+            active: 'none',
+            response: 'null'
         });
 
+    })
+    .post('/send', function (req, res, next) {
+
+        let mail = req.body.mail;
+
+        if(mail){
+
+            dbAcces.insertCustomer(mail).then(result => {
+                res.render('news-letter', {
+                    title: 'stay connect',
+                    active: 'none',
+                    response: 0
+                });
+            }).catch(err => {
+                res.render('news-letter', {
+                    title: 'stay connect',
+                    active: 'none',
+                    response: err
+                });
+            });
+
+        }
+        else{
+            res.render('news-letter', {
+                title: 'stay connect',
+                active: 'none',
+                response: 1
+            });
+        }
     });
 
 module.exports = router;
